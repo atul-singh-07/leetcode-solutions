9ed67bb3a64718830a4896df62b2cd6fdf90eb83
Atul Singh <satul1628@gmail.com>
2026-02-03 23:52:17 +0530

    Add function to rotate array by k positions
    Implement array rotation using reversal algorithm.

--- C/Medium/0189_Rotate_Array.c
+++ C/Medium/0189_Rotate_Array.c
@@ -1,1 +1,16 @@
+void rotate(int* nums, int numsSize, int k) {
+    k=k%numsSize;// for handing large input to k
+    void reverse(int arr[],int si,int ei){
+        for(int i=si,j=ei;i<=j;i++,j--){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+        }
+        return;
+    }
+    // function calling to reverse array
+    reverse(nums,0,numsSize-1);// reversing complete array
+    reverse(nums,0,k-1);// reversing first k elements
+    reverse(nums,k,numsSize-1);// reversing last remaining elements
 
+}
