12bbfff66fbed8394976baf40548635e9f2c983d
Atul Singh <satul1628@gmail.com>
2026-02-04 00:07:27 +0530

    Add rotate function for array rotation by K steps
    Implemented the rotate function to rotate an array by K steps using reversal algorithm.

--- C/Medium/0189_Rotate_Array.c
+++ C/Medium/0189_Rotate_Array.c
@@ -1,3 +1,21 @@
+// Given an integer array nums, rotate the array to the right by K steps, where K is nonnegative.
+/*
+LeetCode 189 - Rotate Array
+Difficulty: Medium
+
+Approach:sabse pehle hamne k=k%10 kiye for handling big values of k then 
+making a fuction that will be inputted by the initial array and [si:starting index ei:ending index] for roating the array by the help of 
+index values
+step01: complete array reverse by reverse(nums,0,numsSize-1)
+step02: reversing the first k elemnt of the new updated array reverse(nums,0,k-1)
+step03: recersing the remaing last element of the array (nums,k,numsSize-1)
+
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+Runtime=0ms
+Memory=26.45MB
+*/
 void rotate(int* nums, int numsSize, int k) {
     k=k%numsSize;// for handing large input to k
     void reverse(int arr[],int si,int ei){
